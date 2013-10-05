c4d7b48563f241ec932c70212c78be7879174134
Deepanjan Roy <deepanjan.roy@mail.mcgill.ca>
2013-10-05 17:34:55 -0400

    Minor cleanup of views

--- imagine/ideastorm/views.py
+++ imagine/ideastorm/views.py
@@ -1,6 +1,5 @@
 # Create your views here.
 
-from django.http import HttpResponse
 from django.shortcuts import render
 from ideastorm.models import Idea
 
@@ -11,7 +10,7 @@ def index(request):
 def idealist(request):
     if request.method == 'POST':
         context = {}
-        new_idea = Idea.objects.create(title=request.POST['title'],
+        Idea.objects.create(title=request.POST['title'],
                                        description=request.POST['description'])
         return render(request, 'ideastorm/index.html', context)
 
